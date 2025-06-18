e9242f35a4b40a331c6b8b01dd87625cdc22a9f5
EzequielAdrianFernandez <ezequieladrianfernandez896@gmail.com>
2025-06-17 22:59:34 -0300

    modularisacion de codigo

---
+++ para_git/el payaso sabe/funciones.py
@@ -0,0 +1,48 @@
+import random 
+
+def randomisar_listas_de_respuestas(diccionario:dict):
+    
+    verdadera = diccionario["res_correcta"][0]
+    falsa_1 , falsa_2 , falsa_3 = diccionario["res_incorrecta"][0],diccionario["res_incorrecta"][1],diccionario["res_incorrecta"][2]
+
+    lista_temp = [verdadera,falsa_1,falsa_2,falsa_3]
+    random.shuffle(lista_temp)
+    return lista_temp,verdadera
+
+
+
+def verificar_respuesta_correcta(ingreso:str,respuestas_mezcladas:list,res_correcta:str):
+    numeros_validos = ["1","2","3","4"]
+    invalidas = 0
+
+    for m in range(len(numeros_validos)):
+
+        if ingreso == numeros_validos[m]:
+            ingreso_int=int(ingreso)
+            ingreso_int = ingreso_int - 1
+            contesto = True
+
+            if respuestas_mezcladas[ingreso_int] == respuestas_mezcladas[res_correcta]:
+                return "correcta!",contesto
+
+            else:
+                return "incorrecta :(",contesto
+
+        else:
+            invalidas += 1
+
+        if invalidas == 4:
+            contesto = False
+            return "ingreso invalido",contesto
+
+
+
+def randomisar_listas_de_respuestas(diccionario:dict):
+    
+    verdadera = diccionario["res_correcta"][0]
+    falsa_1 , falsa_2 , falsa_3 = diccionario["res_incorrecta"][0],diccionario["res_incorrecta"][1],diccionario["res_incorrecta"][2]
+
+    lista_temp = [verdadera,falsa_1,falsa_2,falsa_3]
+    random.shuffle(lista_temp)
+    return lista_temp,verdadera
+
